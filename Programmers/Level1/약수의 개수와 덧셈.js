ce31d0b31a83561b0d62f84989ed6201ccdb53e3
KJeeu <116357790+KJeeu@users.noreply.github.com>
2023-03-16 13:26:11 +0900

    Create : 약수의 개수와 덧셈

---
+++ Programmers/Level1/약수의 개수와 덧셈.js
@@ -0,0 +1,31 @@
+function solution(left, right) {
+    let answer = 0;
+    let count = 0;
+    
+    for(let i = left; i <= right; i++){
+        for(let j = 1; j <= i; j++){
+            if(i % j === 0) count++;
+        } if(count % 2 === 0) {
+            answer += i;
+        } else answer -= i;
+        count = 0;
+    }
+    return answer;
+}
+
+/*
+다른 풀이를 보니
+약수의 개수가 홀수라면 제곱근이 정수라는 공식을 알 수 있었다
+
+ solution(left, right) {
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
+        } else {
+            answer += i;
+        }
+    }
+    return answer;
+}
+*/
