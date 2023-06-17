0a45b4eb171d400c5a33171a0cd4fb7f1a69e13e
Kim JiHo <116357790+KJeeu@users.noreply.github.com>
2023-06-18 00:50:54 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.js
@@ -1,16 +1,22 @@
 function solution(sequence, k) {
-    let [l, r] = [0, 0]
-    let summed = sequence[l]
-    const candidate = []
-    while (r < sequence.length) {
-        if (summed < k) summed += sequence[++r]
-        else if (summed > k) summed -= sequence[l++]
-        else {
-            candidate.push([l, r])
-            summed += sequence[++r]
-            summed -= sequence[l++]
+    let [x, y] = [0, 0];
+    let sum = sequence[x]; //현재 합
+    const list = [];
+    
+    if (sequence.includes(k)) return [sequence.indexOf(k), sequence.indexOf(k)];
+    else {
+        while (y < sequence.length){
+            if (sum < k) {
+                sum += sequence[++y];
+            } else if (sum > k) {
+                sum -= sequence[x++];
+            } else {
+                list.push([x, y]);
+                sum += sequence[++y];
+                sum -= sequence[x++];
+            }
         }
     }
-    // 후보 중 가장 차가 적은 것을 리턴
-    return candidate.sort((a, b) => (a[1] - a[0]) - (b[1] - b[0]))[0] 
+    // list 중 가장 차가 적은 것 return
+    return list.sort((a, b) => (a[1] - a[0]) - (b[1] - b[0]))[0];
 }
