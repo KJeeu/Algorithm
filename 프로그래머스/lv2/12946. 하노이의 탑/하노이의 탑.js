3f70c7352fbed9020f97379220005ef88d0f43a8
Kim JiHo <116357790+KJeeu@users.noreply.github.com>
2023-06-24 06:10:36 +0900

    [level 2] Title: 하노이의 탑, Time: 4.83 ms, Memory: 48.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    const answer = [];
+    
+    const hanoi = (num, from, to, via) => {
+        if(num === 1) {
+            answer.push([from, to]);
+        } else {
+            hanoi(num - 1, from, via, to);
+            answer.push([from, to]);
+            hanoi(num - 1, via, to, from);
+        }
+    }
+    
+    hanoi(n, 1, 3, 2);
+    return answer;
+}
